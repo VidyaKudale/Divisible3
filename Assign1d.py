27e1989f9eacf8494f0a488ad147db471faa48fc
VidyaKudale <68265752+VidyaKudale@users.noreply.github.com>
2020-08-06 12:33:38 +0530

    Delete Assign1d.py

--- Assign1d.py
+++
@@ -1,8 +0,0 @@
-#Assignment 1-d
-print("Enter 2 integer numbers:")
-a=input("First Number:")
-b=input("Second Number:")
-a=int(a)
-b=int(b)
-print("Integer Division =",a//b)
-print("Float Division =",a/b)
