56d9ce612a41a57b536f2d51e1596511937b5fd9
VidyaKudale <68265752+VidyaKudale@users.noreply.github.com>
2020-08-06 12:33:52 +0530

    Delete Assign1c.py

--- Assign1c.py
+++
@@ -1,9 +0,0 @@
-#Assignment 1-c
-print("Enter 2 integer numbers:")
-a=input("First Number:")
-b=input("Second Number:")
-a=int(a)
-b=int(b)
-print("Sum =",a+b)
-print("Difference =",a-b)
-print("Product =",a*b)
